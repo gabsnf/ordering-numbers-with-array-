80e7a276002fee6dbf7fd69a6389466075e6584e
Gabriel Francisco Xavier <91631321+gabsnf@users.noreply.github.com>
2022-05-16 13:51:53 -0300

    Add files via upload

---
+++ arraysort.js
@@ -0,0 +1,45 @@
+// let array = ["lapis", "pincel", 33, 123, false, "chiclete", 25, "chinelo"]
+// for(j=0;j<array.length;j++){
+//     if(array[j]=== true ||array[j]===  false){console.log("tem um booleano na lista")}
+    
+// }
+
+// let array=[5,3,9,7,4,10,1,8,2,6]
+// array.sort()
+// for(j=0;j<array.length;j++){
+//     console.log(array)
+// }
+
+// let array1=["c","e","a","b","d"]
+// array1.sort()
+// console.log(array1)
+
+// let vasco=["vasco",3,0,"rambo",true,false,2]
+// for(j=0;j<vasco.length;j++){
+//     if(vasco[j]=== true || vasco[j]=== false){console.log("jajajaja")}
+// }
+
+
+let array = [1,1,2,1,1,1]
+
+for(k=0;k<array.length;k++){
+    array.sort()
+    let num=array[0]
+    let array3=array.length - 1 
+    let num1=array[array3]
+    console.log(num1)
+    console.log(array3)
+    if(num==array3 || array!=array3){
+        console.log(num1 + " este numero eh o diferente")
+    }
+
+}
+
+// let lol=[4,4,4,4,6,4,4]
+// for(let n=0;n<lol.length;n++){
+//     lol.sort();
+//     let ion=lol[0]
+//     let you=lol.length - 1
+//     let sus=lol[you]
+//     console.log(sus)
+// }
